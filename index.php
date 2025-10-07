faf0f02302245d56361ee66fc00e4d4490a64c42
Agus Aviles <laboratorio.web02@gmail.com>
2025-10-07 16:02:43 -0300

    index con carrusel-agregue navbar con bts

--- index.php
+++ index.php
@@ -9,9 +9,52 @@ $avisos = mysqli_query($conn, "SELECT * FROM aviso ORDER BY id ASC");
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width,initial-scale=1" />
     <title>Inicio</title>
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/carrusel.css">
   </head>
   <body>
+
+  <!-- navbar -->
+
+  <nav class="navbar bg-dark navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <button class="btn btn-sm btn-outline-secondary" type="button">Login</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+<!-- fin navbar -->
 
   <!-- Carrusel -->
   <section class="carousel" id="carousel" aria-label="Carrusel de imágenes">
@@ -50,8 +93,12 @@ $avisos = mysqli_query($conn, "SELECT * FROM aviso ORDER BY id ASC");
       <?php } ?>
     </div>
 
-    <button class="carousel__arrow carousel__arrow--prev" id="prev" aria-label="Anterior">&#9664;</button>
-    <button class="carousel__arrow carousel__arrow--next" id="next" aria-label="Siguiente">&#9654;</button>
+    <button class="carousel__arrow carousel__arrow--prev" id="prev" aria-label="Anterior">
+      <img src="img/izq.svg" alt="flecha izquierda">
+    </button>
+    <button class="carousel__arrow carousel__arrow--next" id="next" aria-label="Siguiente">
+      <img src="img/der.svg" alt="flecha derecha">
+    </button>
     <div class="carousel__dots" id="dots" role="tablist" aria-hidden="false"></div>
   </section>
 
@@ -90,6 +137,6 @@ $avisos = mysqli_query($conn, "SELECT * FROM aviso ORDER BY id ASC");
   </footer>
 
   <script src="js/carrusel.js"></script>
-
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
   </body>
 </html>
